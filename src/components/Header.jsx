c276faa0c056b8e46f9f63ada128996366388f77
mrlaboratory <mrlaboratory23@gmail.com>
2023-05-01 23:08:48 +0600

    added

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>hello test</h2>
+        </div>
+    );
+};
+
+export default Header;
